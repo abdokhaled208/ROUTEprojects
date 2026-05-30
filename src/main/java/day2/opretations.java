e7b0ff191264c9f35a408e974e6a5eede3175608
abdokhaled208 <youngbodda208@gmail.com>
2026-05-30 15:43:10 +0300

    day-2 changes

---
+++ src/main/java/day2/opretations.java
@@ -0,0 +1,123 @@
+package day2;
+
+import java.util.Arrays;
+
+public class opretations {
+    static void main(String[] args) {
+        /*
+        int x =10;
+        int y =20;
+
+        System.out.println(x == y); //true
+        System.out.println(x = y); //x=20
+        System.out.println(" x = " + x);
+        System.out.println(x == y); //false
+        double a =15;
+        int b = 15;
+        System.out.println(a == b); //true
+        System.out.println(a != b); //false
+        int a = 25;
+        System.out.println(a);
+        int b = 25;
+        System.out.println(a + b);
+        a = a + 5;
+
+        a += 5; //shorthand assignment ->
+        System.out.println(a);
+
+        a = a - 5;
+        a -= 5;
+        System.out.println("sub of a - 5 equals = " + a);
+
+        a = a * 5;
+        a *=5;
+        System.out.println("the multi of a * 5 = "+a);
+
+        a = a/ 5;
+        a /=5;
+        System.out.println("the div of a / 5 = " + a);
+        System.out.println("the div of a is: %d%n" , a / 5);
+
+        int x = 20;
+        int y = 3;
+        System.out.println(x % y);
+
+        int a = 7;
+        int b =5;
+        System.out.println(a > b);//t
+        System.out.println(a < b); //f
+        System.out.println(a == b); //f
+        System.out.println(a >= b); //t
+        System.out.println(a <= b); //t
+*/
+/*
+        int a = 7;
+        int b = 5;
+        int c = 7;
+        int d = 7;
+
+        boolean v1 = true;
+        boolean v2 = false;
+
+        System.out.println( v1 && v2); //AND => F
+        System.out.println( v1 || v2); //OR => T
+
+
+
+
+        System.out.println( a > b  && b < c ); // true
+
+        System.out.println( a > b  && b > c ); // false
+
+        System.out.println( a < b  || d > c ); // false
+
+        System.out.println( a <= b  || d >= a ); // True
+
+        int abdi = 20;
+        int elmi =30;
+        boolean tamioz = false;
+
+        if (abdi >= elmi && tamioz )
+        {
+            System.out.println( "أدخل ادبي");
+        }
+        else
+        {
+            System.out.println( "أدخل علمي");
+        }
+
+        int num = 10;
+        System.out.println(num++); // post increment -> num+1
+
+        System.out.println(num); //11
+
+        System.out.println(++num); //11 pre increment
+
+        int number = 5;
+        System.out.println(number++);
+        System.out.println(number);
+        System.out.println(++number);
+        */
+        int a =18 , b =10, c;
+        c = (a > b) ? a: b; //if a > b = true ,then c = a false c=b
+        System.out.println(" the largest value is:" + c);
+        if (a>b) {
+            c =a ;
+
+        } else{
+                c=b;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
